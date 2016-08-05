984d9dc07b233f59d581fb945d66845cb9b65d92
adizon <adizon@evostream.com>
2016-08-05 10:24:22 +0800

    Prevent user extension from aborting

--- azure_templates/ems171_windows2012/activate.rb
+++ azure_templates/ems171_windows2012/activate.rb
@@ -7,7 +7,6 @@ WEB_CONFIG_FILE = "C:\\EvoStream\\config\\webconfig.lua"
 WEB_CONFIG_AUTH_FILE = WEB_CONFIG_FILE + ".auth"
 WEBUI_HTDIGEST_FILE = "C:\\EvoStream\\evo-webroot\\EMS_Web_UI\\settings\\passwords\\.htdigest"
 WEBUI_HTDIGEST_AUTH_FILE = WEBUI_HTDIGEST_FILE + ".auth"
-DOWNLOAD_FOLDER = "C:\\Users\\evostream\\Downloads"
 
 def get_instance_id
   result = `wmic csproduct get uuid`.split[1]
@@ -19,8 +18,16 @@ def replace_proxy_password
     instance_id = get_instance_id
     `type #{WEB_CONFIG_AUTH_FILE} | ruby -pe "gsub '__STANDARD_PROXY_DOMAIN__', '#{PROXY_DOMAIN}'" | \
         ruby -pe "gsub '__STANDARD_PROXY_PASSWORD__', '#{instance_id}'" > #{WEB_CONFIG_FILE}`
-    FileUtils.cp WEB_CONFIG_AUTH_FILE, DOWNLOAD_FOLDER
-    FileUtils.rm WEB_CONFIG_AUTH_FILE
+    begin
+      FileUtils.cp WEB_CONFIG_AUTH_FILE, "."
+    rescue Exception => e
+      puts "Error: #{e}"
+    end
+    begin
+      FileUtils.rm WEB_CONFIG_AUTH_FILE
+    rescue Exception => e
+      puts "Error: #{e}"
+    end
     puts "Password = #{instance_id}"
   else
     puts "Missing file '#{WEB_CONFIG_AUTH_FILE}'!" if VERBOSE
@@ -36,8 +43,16 @@ def replace_webui_password
     out_file.puts "evostream:#{password}"
     out_file.puts "user1000:#{password}"
     out_file.close
-    FileUtils.cp WEBUI_HTDIGEST_AUTH_FILE, DOWNLOAD_FOLDER
-    FileUtils.rm WEBUI_HTDIGEST_AUTH_FILE
+    begin
+      FileUtils.cp WEBUI_HTDIGEST_AUTH_FILE, "."
+    rescue Exception => e
+      puts "Error: #{e}"
+    end
+    begin
+      FileUtils.rm WEBUI_HTDIGEST_AUTH_FILE
+    rescue Exception => e
+      puts "Error: #{e}"
+    end
     puts "Password = #{instance_id}"
   else
     puts "Missing file '#{WEBUI_HTDIGEST_AUTH_FILE}'!" if VERBOSE
